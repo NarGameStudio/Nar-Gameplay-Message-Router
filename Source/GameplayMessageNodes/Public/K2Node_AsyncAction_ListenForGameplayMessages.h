1195bdd810835cba96ca17b53da274825f5bb93e
Can Göktaş <cangoktas@nargamestudio.com>
2024-11-11 19:20:25 +0300

    Fixed include for 5.4

--- Source/GameplayMessageNodes/Public/K2Node_AsyncAction_ListenForGameplayMessages.h
+++ Source/GameplayMessageNodes/Public/K2Node_AsyncAction_ListenForGameplayMessages.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
-#include "BlueprintGraph/Classes/K2Node_AsyncAction.h"
 #include "CoreMinimal.h"
+#include "K2Node_AsyncAction.h"
 #include "UObject/ObjectMacros.h"
 
 #include "K2Node_AsyncAction_ListenForGameplayMessages.generated.h"
@@ -24,26 +24,18 @@ class UK2Node_AsyncAction_ListenForGameplayMessages : public UK2Node_AsyncAction
 	virtual void PinDefaultValueChanged(UEdGraphPin* ChangedPin) override;
 	virtual void GetPinHoverText(const UEdGraphPin& Pin, FString& HoverTextOut) const override;
 	//~End of UEdGraphNode interface
-	
+
 	//~UK2Node interface
 	virtual void GetMenuActions(FBlueprintActionDatabaseRegistrar& ActionRegistrar) const override;
 	virtual void AllocateDefaultPins() override;
 	//~End of UK2Node interface
 
 protected:
-	virtual bool HandleDelegates(
-		const TArray<FBaseAsyncTaskHelper::FOutputPinAndLocalVariable>& VariableOutputs, UEdGraphPin* ProxyObjectPin,
-		UEdGraphPin*& InOutLastThenPin, UEdGraph* SourceGraph, FKismetCompilerContext& CompilerContext) override;
+	virtual bool HandleDelegates(const TArray<FBaseAsyncTaskHelper::FOutputPinAndLocalVariable>& VariableOutputs, UEdGraphPin* ProxyObjectPin, UEdGraphPin*& InOutLastThenPin, UEdGraph* SourceGraph, FKismetCompilerContext& CompilerContext) override;
 
 private:
-	
 	// Add the GetPayload flow to the end of the delegate handler's logic chain
-	bool HandlePayloadImplementation(
-		FMulticastDelegateProperty* CurrentProperty,
-		const FBaseAsyncTaskHelper::FOutputPinAndLocalVariable& ProxyObjectVar,
-		const FBaseAsyncTaskHelper::FOutputPinAndLocalVariable& PayloadVar,
-		const FBaseAsyncTaskHelper::FOutputPinAndLocalVariable& ActualChannelVar,
-		UEdGraphPin*& InOutLastActivatedThenPin, UEdGraph* SourceGraph, FKismetCompilerContext& CompilerContext);
+	bool HandlePayloadImplementation(FMulticastDelegateProperty* CurrentProperty, const FBaseAsyncTaskHelper::FOutputPinAndLocalVariable& ProxyObjectVar, const FBaseAsyncTaskHelper::FOutputPinAndLocalVariable& PayloadVar, const FBaseAsyncTaskHelper::FOutputPinAndLocalVariable& ActualChannelVar, UEdGraphPin*& InOutLastActivatedThenPin, UEdGraph* SourceGraph, FKismetCompilerContext& CompilerContext);
 
 	// Make sure the output Payload wildcard matches the input PayloadType 
 	void RefreshOutputPayloadType();
